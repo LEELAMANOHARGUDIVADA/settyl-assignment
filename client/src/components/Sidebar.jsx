1b0767a4fb3877b309eb48e9ca3b122f4ac5d4e2
LEELA MANOHAR GUDIVADA <142682424+LEELAMANOHARGUDIVADA@users.noreply.github.com>
2024-10-30 20:48:07 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import threads from "../assets/threads.png";
-import threads_light from "../assets/threads-light.png";
+import threads_light from "../assets/threads-light.png"
 import { GoHeart, GoHome } from "react-icons/go";
 import { HiMagnifyingGlass } from "react-icons/hi2";
 import { FaPlus, FaRegUser } from "react-icons/fa6";
@@ -27,11 +27,11 @@ const Sidebar = () => {
   return (
     <div className="hidden sm:w-1/4 md:w-2/4 lg:w-full h-screen sm:flex flex-col justify-between">
       <Link to={`/`} className="relative left-8 top-4">
-        {<img
+        <img
           src={theme === "dark" ? threads : threads_light}
-          alt="threads-clone"
-          className="w-9 hover:scale-105 transform duration-150"
-        />};
+          alt="threads"
+          className="w-10 hover:scale-105 transform duration-150"
+        />
       </Link>
 
       <ul className="mt-8 space-y-5 font-semibold">
