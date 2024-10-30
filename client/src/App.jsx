93566d8963e8860fe62822420402ab638f48bc68
LEELA MANOHAR GUDIVADA <142682424+LEELAMANOHARGUDIVADA@users.noreply.github.com>
2024-10-30 18:51:32 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { ThemeProvider } from './components/theme-provider'
 import { AuthProvider } from './context/AuthContext'
 import Routers from './routes/Routers'
 import { Toaster } from "@/components/ui/toaster"
@@ -8,12 +7,10 @@ import { Toaster } from "@/components/ui/toaster"
 function App() {
 
   return (
-    <ThemeProvider>
       <AuthProvider>
       <Routers />
       <Toaster position="top-right" />
       </AuthProvider>
-    </ThemeProvider>
   )
 }
 
